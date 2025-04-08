d4f31822a5523b5b1cdee083a47ac75236353fcc
krvt <tabiquerokhristopher1@gmail.com>
2025-04-08 22:53:13 +0800

    sliding images

--- javascript.js
+++ javascript.js
@@ -1,4 +1,13 @@
-function sayHello() {
-    alert("Hello from MDC-inspired site!");
-  }
-  
+let current = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.toggle("active", i === index);
+  });
+}
+
+setInterval(() => {
+  current = (current + 1) % slides.length;
+  showSlide(current);
+}, 4000); // changes every 4 seconds
